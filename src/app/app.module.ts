43ff7c66f638c6426b3661a6d906f501ca663dc0
Obiorah Cyril <cyobiorah@gmail.com>
2018-04-17 09:35:30 +0100

    Add user registration and store registration pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,73 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { NewArrivalsComponent } from './new-arrivals/new-arrivals.component';
+import { ForHerComponent } from './for-her/for-her.component';
+import { ForHimComponent } from './for-him/for-him.component';
+import { SaleComponent } from './sale/sale.component';
+import { PartnersComponent } from './partners/partners.component';
+import { FooterComponent } from './footer/footer.component';
+import { StoreRegisterComponent } from './store-register/store-register.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'index',
+    pathMatch: 'full'
+  },
+  {
+    path: 'index',
+    component: HomeComponent
+  },
+  {
+    path: 'storeregister',
+    component: StoreRegisterComponent
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    NewArrivalsComponent,
+    ForHerComponent,
+    ForHimComponent,
+    SaleComponent,
+    PartnersComponent,
+    FooterComponent,
+    StoreRegisterComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } //Debug purpose only
+    )
   ],
+  exports: [RouterModule],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
